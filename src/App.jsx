e642a203ce38808644793f3150be5cdf5f88a8e3
NgocDungK68 <dungtranngoc1412@gmail.com>
2026-02-10 13:56:57 +0700

    init ecommerce FE project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Products from './components/products/Products.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Products />
+    </div>
+  )
+}
+
+export default App
